72dabfbc09e386f1285bf1b43a1bf729c6f1ed00
Geoloqi Server Apollo <dev@geoloqi.com>
2011-10-26 21:02:18 +0000

    Updates for renamed fields in infochimps api

--- fetch.rb
+++ fetch.rb
@@ -35,30 +35,30 @@ info['locations'].each do |location|
   data = JSON.parse response.body
   
   data['results'].each{|article|
-    
-    if pages.include? article['title']+"\n" 
+
+    if pages.include? article['name']+"\n" 
       print '-'
       next
     end
 
-    pages.push article['title']+"\n"
+    pages.push article['name']+"\n"
   
-    # puts article['url'] + ' ' + article['abstract']
+    # puts article['url'] + ' ' + article['about']
     
     # Figure out an appropriate radius
     radius = 400
     
-    if article['abstract'] =~ /is an? [\w ]*neighborhood|is an? \w* ?area/
+    if article['about'] =~ /is an? [\w ]*neighborhood|is an? \w* ?area/
       radius = 800
-    elsif article['abstract'] =~ /is an? \w* ?park/
+    elsif article['about'] =~ /is an? \w* ?park/
       radius = 400
-    elsif article['abstract'] =~ /is an? \w* ?building/
+    elsif article['about'] =~ /is an? \w* ?building/
       radius = 120
     end
     
     response = geoloqi.post 'place/create', {
       :layer_id => layerID,
-      :name => article['title'], 
+      :name => article['name'], 
       :latitude => article['coordinates'][1],
       :longitude => article['coordinates'][0],
       :radius => radius,
@@ -68,12 +68,12 @@ info['locations'].each do |location|
     if geoloqi.response.status == 200   # api returns 409 if the place already exists
       newArticles = newArticles + 1
       puts
-      puts article['url']
+      puts article
       puts response
       puts geoloqi.post 'trigger/create', {
         :place_id => response['place_id'],
         :type => 'message',
-        :text => article['abstract'],
+        :text => article['about'],
         :url => article['url'],
         :one_time => 1
       }
@@ -93,7 +93,7 @@ info['locations'].each do |location|
 #         puts triggers
 #         trigger = triggers['triggers'][0]
 #         puts geoloqi.post 'trigger/update/' + trigger['trigger_id'], {
-#           :text => article['abstract']
+#           :text => article['about']
 #         }
 #       end
     else 
